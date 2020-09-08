ff1244ce45bf2be6af6b2efa8ae466b9dd078fe5
andreas-repo <andreas.mayer@gepardec.com>
2020-09-08 21:59:32 +0200

    --Text to Speech translation of input added

--- app/src/main/java/de/androidnewcomer/saghallo/MainActivity.java
+++ app/src/main/java/de/androidnewcomer/saghallo/MainActivity.java
@@ -2,12 +2,15 @@ package de.androidnewcomer.saghallo;
 
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, View.OnClickListener {
 
     private TextToSpeech textToSpeech;
 
@@ -21,7 +24,15 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
     @Override
     public void onInit(int i) {
-        textToSpeech.speak("Hallo Ulrike wie gehts?", TextToSpeech.QUEUE_FLUSH, null);
+        Button button = findViewById(R.id.submitHintButton);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
         textToSpeech.setLanguage(Locale.GERMAN);
+        EditText editText = findViewById(R.id.editTextInputHint);
+        textToSpeech.speak( editText.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+
     }
 }
